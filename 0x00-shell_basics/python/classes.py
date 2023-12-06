f6b3443a33513304026879d9f7ce6bba2aef1677
Yabiti <yabetseyohannes@gmail.com>
2023-12-06 14:06:12 +0300

    classes

--- 0x00-shell_basics/python/classes.py
+++ 0x00-shell_basics/python/classes.py
@@ -14,4 +14,4 @@ Flight = flight(3)
 people = ["harry", "hermone", "ron", "draco"]
 for person in people:
     if flight,add_passenger(person):
-        print("passenger added to {person} successfully")
+        print("passenger added {person} to successfully")
