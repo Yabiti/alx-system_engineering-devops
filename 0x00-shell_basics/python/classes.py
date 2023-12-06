aac69d74430fc729a0854b161507ae5a3fdd8b4e
Yabiti <yabetseyohannes@gmail.com>
2023-12-06 14:12:36 +0300

    classes

--- 0x00-shell_basics/python/classes.py
+++ 0x00-shell_basics/python/classes.py
@@ -2,18 +2,19 @@ class Flight:
     def __init__(self,capacity):
         self.capacity = capacity
         self.passengers = []
+
     def add_passengers(self, name):
         if not self.open_seats():
             return False
         self.passengers.append(name)
-            return True
+        return True
     def open_seats(self):
         return self.capacity - len(self.passengers)
 Flight = flight(3)
 
 people = ["harry", "hermone", "ron", "draco"]
 for person in people:
-    if flight,add_passenger(person):
+    if flight.add_passenger(person):
         print("passenger added {person} to successfully")
     else:
         print("no available seats for {person}")
