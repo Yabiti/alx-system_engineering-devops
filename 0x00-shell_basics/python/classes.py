685c3724bccada79891e235a98ff4e3cc5dc4a09
Yabiti <yabetseyohannes@gmail.com>
2023-12-06 13:44:46 +0300

    classes

--- 0x00-shell_basics/python/classes.py
+++ 0x00-shell_basics/python/classes.py
@@ -5,4 +5,7 @@ class Fight:
 
     def add_passengers(self, name):
         self.passengers.append(name)
+
+    def open_seats(self):
+        return self.capacity - self.passengers
 Flight = flight(3):
