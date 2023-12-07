0bfa2d3b418d02d364d575d072435c41ba82d668
Yabiti <yabetseyohannes@gmail.com>
2023-12-07 14:30:59 +0300

    decorators

--- 0x00-shell_basics/python/exceptions.py
+++ 0x00-shell_basics/python/exceptions.py
@@ -6,6 +6,6 @@ y = int(input("what's y? "))
 try:
     result = x / y
 except ZeroDivisionError:
-    print("ERROR: cannot be divided by zero")
+    print("ERROR: cannot be divided by 0")
     sys.exit(1)
 print(f" {x} / {y} = {result}")
