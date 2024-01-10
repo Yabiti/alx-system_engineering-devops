f52edaa01e1596ef56d313681b35de76536a6873
Yabiti <yabetseyohannes@gmail.com>
2024-01-10 12:45:54 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,5 +1,1 @@
-var multiply = 50 * 10
-var divide = 50 / 10
-
-console.log("fifty times ten is", multiply)
-console.log("fifty divide by ten is", divide)
+var text = "hry there is some mother fuckers around here"
