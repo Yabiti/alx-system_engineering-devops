53c6b6d5eee610f497be7ede6b87e307f16a9a73
Yabiti <yabetseyohannes@gmail.com>
2024-01-12 16:46:07 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -2,9 +2,8 @@ arraysum([4 , 5 ,6 ,7])
 arraysum([-6, 10 ,0 ,4])
 
 var arraysum = function(array){
-    varsum = 0;
-    for(i = 0; i <= length.array; I++){
+    var sum = 0;
+    for(var i = 0; i < array.length; i++){
         sum = sum + array[i];
     }
-    return sum;
 }
