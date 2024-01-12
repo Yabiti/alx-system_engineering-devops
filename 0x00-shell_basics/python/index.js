d3445d51efb04887a52d6d24d8f36dd5756fb22c
Yabiti <yabetseyohannes@gmail.com>
2024-01-12 10:20:54 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,7 +1,1 @@
-var skycolor = "blue";
-var haircolor = "blonde";
-var carcolor = "blue";
-
-console.log(carcolor === skycolor);
-console.log(skycolor === haircolor);
-console.log(haircolor !== skycolor);
+var oneplustwo = sum(1, 2);
