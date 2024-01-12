9f5e3b0ad3fa5a79f099be4b60f69cbe26ec9ab1
Yabiti <yabetseyohannes@gmail.com>
2024-01-12 10:03:38 +0300

    /

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,8 +1,1 @@
-var word1 = "billy";
-var word2 = "got";
-var word3 = "to go";
-var space = " ";
-var period = ".";
-
-var sentence = word1 + space + word2 + space + word3 + period
-console.log(sentence)
+var skycolor = blue;
