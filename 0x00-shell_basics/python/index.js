c4a8962400f18458a53146b69cd84751541b7412
Yabiti <yabetseyohannes@gmail.com>
2024-01-12 14:03:24 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,8 +1,10 @@
 var num = 0;
 
-while(num <=10);
+while(num <=10){
 if(num !== 10){
     console.log("the number is", num,"lessthan 10");
 } else {
     console.log("the number is", num , "the loop is now over");
+}
+    num++;
 }
