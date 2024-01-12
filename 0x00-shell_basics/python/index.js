a82fab34081ccc11b284790876ce153a6cb2d32d
Yabiti <yabetseyohannes@gmail.com>
2024-01-12 17:10:00 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,9 +1,6 @@
-arraysum([4 , 5 ,6 ,7])
-arraysum([-6, 10 ,0 ,4])
-
-var arraysum = function(array){
-    var sum = 0;
-    for(var i = 0; i < array.length; i++){
-        sum = sum + array[i];
-    }
-}
+var students = [
+   ["marry", "female", 10],
+   ["hellen", "female", 15],
+   ["yabetse", "Female, 17"]
+];
+console.log(students);
