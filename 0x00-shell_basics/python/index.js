06f5eae4d73addc8bd7e23df9c3785c099e27c74
Yabiti <yabetseyohannes@gmail.com>
2024-01-12 14:54:37 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,17 +1,18 @@
 var rangesum = function(number){
     var sum = 0;
-    var i = number;
-    if(number <0 ){
-        while(i < 0){
-            aum = sum + i;
-            i = i + 1;
-        }
-    }
-    else {
-        while(i > 0){
-            aum = sum + i;
-            i = i - 1;
-        }
+   var i = number;
+   if(number < 0) {
+    while(i < 0){
+        sum = sum + 1;
+        i = i + 1;
     }
     
+   }
+   else {
+    while(i > 0){
+        sum = sum + 1;
+        i = i - 0;
+    }
+   }
+   return sum;
 }
