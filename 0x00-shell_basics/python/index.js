d67ec279ba0ec2c13e06c2b45bada6abd0478c42
Yabiti <yabetseyohannes@gmail.com>
2024-01-13 14:02:26 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,10 +1,13 @@
 var weather = function(temprature){
-    console.log("the temprature outside is", temprature, "degree celcius")
-    if(weather <=65){
-        console.log("it's getting hot today")
-    } else {
+    console.log("the temprature outside is", temprature);
+    if(temprature <= 30){
+        console.log("the temprature too cold")
+    } else if(temprature <= 50){
         console.log("it's pleasant outside")
+    } else {
+        console.log("it's getting hot outside")
     }
 }
-
-weather(66);
+weather(25);
+weather(35);
+weather(70);
