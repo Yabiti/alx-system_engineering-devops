2e8db251796f416ca1f50d999c39b2db7d502f31
Yabiti <yabetseyohannes@gmail.com>
2024-01-13 14:51:41 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,8 +1,5 @@
-for(var i = 0; i <=1; i++){
-    console.log("always being me:", i);
-    for(j = 0; j <=0; j++){
-        console.log("\ttrying to prove myself:", j);
-        console.log("\tgetting out of crowded areas:", j);
-        console.log("\tbeing there out for me:", j);
-    }
-}
+var students = [
+    ["marry", 10],
+    ["Hellen", 13],
+    ["yabetse", 15]
+];
