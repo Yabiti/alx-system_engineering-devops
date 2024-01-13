d6ed2ba68091a46ac4be4353eb2ae05ff7904d17
Yabiti <yabetseyohannes@gmail.com>
2024-01-13 12:11:17 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,13 +1,5 @@
-var students = [
-    ["marry", 10],
-    ["Hellen", 15],
-    ["yabu", 17]
-];
-
-var printStudents = function(students){
-    for (var i = 0; i <students.length; i++){
-        console.log("student" + i + ":");
-        console.log(students[i][0]);
-        console.log(students[i][1]);
-    }
+var weather = function(temprature){
+    console.log("the temprature is too hot", temprature);
 }
+
+weather(60);
