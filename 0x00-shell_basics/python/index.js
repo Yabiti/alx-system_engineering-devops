82500e7dfc363bdb9616cda3aa567fb713fca0e4
Yabiti <yabetseyohannes@gmail.com>
2024-01-13 14:42:41 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,3 +1,8 @@
-for (var i = 97; i <=100; i++){
-    console.log(i);
+for(var i = 0; i <=1; i++){
+    console.log("always being me", i);
+    for(j = 0; j <=0; j++){
+        console.log("\ttrying to prove myself", j);
+        console.log("\tgetting out of crowded areas", j);
+        console.log("\tbeing there out for me", j);
+    }
 }
