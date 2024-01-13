3a5bb1928e144d68d20fd828b692e3199f6519c6
Yabiti <yabetseyohannes@gmail.com>
2024-01-13 12:18:00 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,5 +1,8 @@
 var weather = function(temprature){
     console.log("the temprature is too hot", temprature);
+    if (temprature <= 65){
+        console.log("the temprature is too cold better wear a jacket");
+    }
 }
 
 weather(60);
