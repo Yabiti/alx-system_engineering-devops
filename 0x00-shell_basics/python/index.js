29b06d35504477115705598420c03d6d51c93229
Yabiti <yabetseyohannes@gmail.com>
2024-01-16 11:19:34 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -2,9 +2,11 @@ var operations = {
     sum: function(x, y){return x + y; },
     divide: function(x,y){return x / y},
     multiply: function(x,y){return x * y},
-    subtract: function(x,y){return x - y}
+    subtract: function(x,y){return x - y},
+    module: function(x,y){return x % y}
 }
 console.log(operations.multiply(5,10));
 console.log(operations.divide(10, 5));
 console.log(operations.subtract(10, 15));
 console.log(operations.sum(10,5));
+console.log(operations.module(5,6));
