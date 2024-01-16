4f48b32070b23fbc1cf50d2f6a417f2d7317a232
Yabiti <yabetseyohannes@gmail.com>
2024-01-16 15:56:29 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,24 +1,10 @@
-var sellStock = function(stockPrice, sellPrice){
-    //write your code here.
-    console.log("the price is",sellPrice);
-    var sell = {
-        sellPrice: stockPrice
+var num = 0;
+
+while(num <= 10){
+    if(num !== 10){
+        console.log("the number is", num , "is lessthan 10");
+    } else {
+        console.log("the number is", num , "loop is over now");
     }
-   
-    if (sellPrice <= 200){
-        console.log("not fair price");
-    } else if(sellPrice <= 400){
-        console.log("not a fair price");
-    } else if (sellPrice >= 600){
-        console.log("sell")
-    } else if(sellPrice >= 800){
-        console.log(sell);
-    } else if(sellPrice >= 1000){
-        console.log("sell");
-    }
-  }
-  sellStock(150, 200);
-  sellStock(350, 400);
-  sellStock(500,600);
-  sellStock(700, 800);
-  sellStock(950, 1000);
+    num++;
+}
