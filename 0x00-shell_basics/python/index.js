325b4906bdec349d116ed7cb205985bf5c85d1f3
Yabiti <yabetseyohannes@gmail.com>
2024-01-16 14:06:42 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,11 +1,7 @@
-var multiply = 10 * 10;
-var divide = 10 / 10;
-var sum = 10 + 5;
-var substract = 15 -10;
-var float = 15 / 13;
+var num = 10;
 
-console.log("ten times ten is ", multiply);
-console.log("ten divide by ten is ", divide);
-console.log("ten plus ten is", sum);
-console.log("fifteen substracted by five is", substract);
-console.log("fifteen divide by thirteen is ", float);
+num++;
+console.log(num);
+
+num--;
+console.log(num);
