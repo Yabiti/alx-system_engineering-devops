5d69d9d4f92d2b1ada8366e0e26162da2f35734c
Yabiti <yabetseyohannes@gmail.com>
2024-01-16 10:51:15 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,12 +1,6 @@
 var student = {
-    name: "marry",
-    age: 55,
+    name: "yabetse",
+    age: 24
 
-    grades: {
-    quiz1: 55,
-    quiz2: 75,
-    quiz3: 95
-    }
+    
 }
-
-console.log(student.grades.quiz1);
