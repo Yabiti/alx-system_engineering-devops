2ade95110c248565404a2f23f76918353d75047c
Yabiti <yabetseyohannes@gmail.com>
2024-01-16 12:12:00 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,13 +1,10 @@
-var createstudent = function(name, age) {
+var createstudent = function(name, age){
     var student = {
         name: name,
-        age: age
+        age: age,
+        birhtday: function(){
+            this.age++;
+        }
     }
     return student;
 }
-
-var student1 = createstudent("yabu", 10);
-var student2 = createstudent("mikey", 15)
-
-console.log(student1.name, student1.age);
-console.log(student2.name, student2.age);
