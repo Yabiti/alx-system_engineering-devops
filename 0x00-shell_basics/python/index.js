96dc10a4d0e855999908546c3b551fb60d4046ae
Yabiti <yabetseyohannes@gmail.com>
2024-01-17 10:00:06 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,10 +1,8 @@
-var students = [
-    ["marry", 10],
-    ["hellen", 15],
-    ["mile", 17]
-]
-for(i = 0; i < students.length; i++){
-    console.log("students " + i + ":");
-    console.log(students[i][0]);
-    console.log(students[i][1]);
+var student = {
+    name: "Marry",
+    age: 15
 }
+
+console.log(student.name, student.age);
+delete student.name;
+console.log(student.name);
