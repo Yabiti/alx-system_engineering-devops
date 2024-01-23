37db6904035af74c68fb0a9f8cab73661eb19b64
Yabiti <yabetseyohannes@gmail.com>
2024-01-23 11:29:38 +0300

    .

--- 0x00-shell_basics/python/dom.js
+++ 0x00-shell_basics/python/dom.js
@@ -1,5 +1,2 @@
-var h1 = document.createElement('h1');
-h1.innerHTML = "get off";
-
 var clean = document.querySelector("#clean");
-clean.appendChild('h1');
+var cleanparagrapgh = document.querySelector("#clean > p");
