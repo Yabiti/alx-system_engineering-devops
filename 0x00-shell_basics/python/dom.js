8031f1d7506093b4f03caee3a06d093b9741699d
Yabiti <yabetseyohannes@gmail.com>
2024-01-23 11:40:04 +0300

    .

--- 0x00-shell_basics/python/dom.js
+++ 0x00-shell_basics/python/dom.js
@@ -1,2 +1,11 @@
-var u1 = document.createElement('li')
-li.innerHTML = "unordered list"
+var u1 = document.createElement('ul')
+var li1 = document.createComment('li');
+li1.innerHTML = "unordered list";
+
+var u1 = document.createElement('ul')
+var li2 = document.createComment('li');
+li2.innerHTML = "unordered list";
+
+var u1 = document.createElement('ul')
+var li3 = document.createComment('li');
+li3.innerHTML = "unordered list";
