a8912bf7384ec88bedd0b9e16f5401df18a0134d
Yabiti <yabetseyohannes@gmail.com>
2024-01-23 10:54:27 +0300

    .

--- 0x00-shell_basics/python/dom.js
+++ 0x00-shell_basics/python/dom.js
@@ -1,10 +1,10 @@
 var headings = document.getElementsByTagName('h1');
-var paragraph = document.getElementsByTagName('p');
+var paragraphs = document.getElementsByTagName('p');
 
 for(var i = 0; i < headings.length; i++){
     headings[i].style.color = "green";
 }
 
 for(var i = 0; i <paragraphs.length; i++){
-    
+    paragraphs[i].style.color = "blue";
 }
