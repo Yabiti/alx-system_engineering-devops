ab63bdd07127f8b93efa46cd0d600968c560959b
Yabiti <yabetseyohannes@gmail.com>
2024-01-23 11:54:36 +0300

    .

--- 0x00-shell_basics/python/dom.js
+++ 0x00-shell_basics/python/dom.js
@@ -1,3 +1,14 @@
 var ul = document.createElement('ul');
 var li1 = document.createElement('li');
 li1.innerHTML = "list one";
+
+var ul = document.createElement('ul');
+var li3 = document.createElement('li');
+li1.innerHTML = "list two";
+
+var ul = document.createElement('ul');
+var li3 = document.createElement('li');
+li1.innerHTML = "list three";
+
+var clean = document.querySelector('body');
+clean.appendChild(ul);
