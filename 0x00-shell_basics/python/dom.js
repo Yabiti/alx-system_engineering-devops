c071e225d84d7374c7c14da3d984a485c6f66b51
Yabiti <yabetseyohannes@gmail.com>
2024-01-23 10:43:40 +0300

    .

---
+++ 0x00-shell_basics/python/dom.js
@@ -0,0 +1,3 @@
+var mainHeading = document.getElementById('mainHeading');
+
+mainHeading.innerHTML = "Access an element with an id attribute using <code>document.getElementById()</code>";
--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,1 +1,5 @@
 var todoItems = document.querySelectorAll('#todos > li');
+
+for(var i = 0; i < todoItems.length; i++) {
+  todoItems[i].innerHTML = "Todo Item #" + (i+1);
+}
