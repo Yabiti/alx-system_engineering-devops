50c0fbfd274597ad64686ed0357a8b019a243101
Yabiti <yabetseyohannes@gmail.com>
2024-01-26 10:17:36 +0300

    .

--- 0x00-shell_basics/python/index.js
+++
@@ -1,8 +0,0 @@
-name = "baily";
-age = 35;
-school = "GCA";
-grade = "A"
-period = ".";
-
-student = "my name is " + name + " and my age is " + age + " , my grade is "+  age  + " period" + period
-console.log(student);
