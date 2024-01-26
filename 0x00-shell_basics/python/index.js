18b257ee817c224478e53d8ccfdcce0ccb08b6fb
Yabiti <yabetseyohannes@gmail.com>
2024-01-26 10:49:59 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -2,7 +2,7 @@ i = 10;
 
 while(i <= 10){
   if(i !== 10){
-    console.log("the number is " + num + "the loop is over");
+    console.log("the number is " + num + " now the loop is over");
     i = i + 1;
   }
   i++;
