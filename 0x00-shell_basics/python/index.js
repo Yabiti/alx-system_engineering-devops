2107c6ae40d405df06748281f37096a8f56406e6
Yabiti <yabetseyohannes@gmail.com>
2024-01-26 13:51:41 +0300

    .

--- 0x00-shell_basics/python/index.js
+++ 0x00-shell_basics/python/index.js
@@ -1,11 +1,15 @@
 var students = [
-    ["marry", 10],
-    ["hellen", 15],
-    ["barbie", 17]
+    ["marry","femlae", 10],
+    ["else","femlae", 15],
+    ["common","femlae", 25]
 ];
 
 var printStudents = function(students){
     for(var i = 0; i < students.length; i++){
-        console.log(students[i]);
+        console.log('studnet' + i + ".");
+        for(var j = 0; j < students[i].length; j++){
+            console.log(students[i][j]);
+        }
     }
 }
+printStudents(students);
