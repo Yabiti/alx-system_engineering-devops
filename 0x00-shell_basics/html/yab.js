baa7efdd7e95d7a2bb4c318a4f21c227f4e1372d
Yabiti <yabetseyohannes@gmail.com>
2024-01-28 00:01:25 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -2,9 +2,9 @@ var headings = document.getElementsByTagName('h1');
 var paragraphs = document.getElementsByTagName('p');
 
 for(var i = 0; i < headings.length; i++){
-    headings[i].style.color = "green";
+    headings[i].style.color = "white";
 }
 
-for(var i = 0; i < headings.length; i++){
-    headings[i].style.color = "green";
+for(var i = 0; i < paragraphs.length; i++){
+    paragraphs[i].style.color = "blue";
 }
