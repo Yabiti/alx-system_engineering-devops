b51d5aa33bd8b563efbf0bc57b2a7a0316052e82
Yabiti <yabetseyohannes@gmail.com>
2024-01-28 00:00:21 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,2 +1,10 @@
 var headings = document.getElementsByTagName('h1');
 var paragraphs = document.getElementsByTagName('p');
+
+for(var i = 0; i < headings.length; i++){
+    headings[i].style.color = "green";
+}
+
+for(var i = 0; i < headings.length; i++){
+    headings[i].style.color = "green";
+}
