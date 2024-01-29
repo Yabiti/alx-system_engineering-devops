498e0ca9e8128c5bfb4302cc34bc52e1fc9dadc9
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 12:27:43 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,7 +1,3 @@
 var h1 = document.querySelector('h1');
 
 var input = document.querySelector('input[type=text]');
-
-console.log("Attribute names", input.getAttributeNames());
-
-input.setAttribute('type', "checkbox");
