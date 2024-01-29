001172ed50d7ac087669ac7679858a2c3da0d2eb
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 15:35:35 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -2,4 +2,5 @@ var randomcolor = function(){
     rvalue = function(){
         return Math.round(Math.random()*255);
     }
+    return 'rgb(' + rvalue() + "," + rvalue() + "," + rvalue() + ")";
 }
