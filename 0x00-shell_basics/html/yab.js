e6304098e601f4cdcc24480a8f3c500c01712e76
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 21:48:44 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,2 +1,10 @@
 var checkstatus = document.getElementById('checkstatus');
 var checkbox = document.getElementById('checkbox');
+
+checkbox.onchange = function(){
+    if(this.checked) {
+        checkstatus.innerHTML = "chceked!";
+    } else {
+        checkstatus.innerHTML = "not chceked!";
+    }
+}
