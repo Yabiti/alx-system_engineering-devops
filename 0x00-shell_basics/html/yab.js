9f43c626a914186ba1616a90833e98a0df98901a
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 12:29:12 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,3 +1,7 @@
 var h1 = document.querySelector('h1');
 
 var input = document.querySelector('input[type=text]');
+
+console.log('attribute names', input.getAttributeNames());
+
+input.setAttribute('type', "radio");
