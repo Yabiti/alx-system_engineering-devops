2bfd220bf015d57401a02436a880ef4cc03b3082
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 22:39:14 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -3,4 +3,7 @@ var button = document.querySelector('body');
 button.onclick = function(){
     var p = document.createElement('p');
     p.innerHTML = "Checked!";
+
+    var content = document.querySelector('body');
+    content.appendChild(p);
 }
