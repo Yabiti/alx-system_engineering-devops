bfe20c506bcf633d3d1ab06491d101286f0ab1df
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 12:55:57 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -2,4 +2,6 @@ var button = document.querySelector('button');
 button.onclick = function(){
 var p = document.createElement('p');
 p.innerHTML = "clicked!";
+var content = document.querySelector('body');
+content.appendChild(p);
 }
