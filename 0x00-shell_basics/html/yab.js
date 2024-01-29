507869718351d1047a59f9bbdd91706364617cce
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 22:51:38 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,6 +1,7 @@
-var button = document.querySelector(button);
+var button = document.querySelector('button');
+
 button.onclick = function(){
-    var p = document.createElement('p');
+    var p = document.querySelector('p');
     p.innerHTML = "Check me out!";
 
     var content = document.querySelector('body');
