b5da6c95cc406ab53ee8b63196e1ff87ca8255ed
Yabiti <yabetseyohannes@gmail.com>
2024-01-29 12:17:46 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,4 +1,3 @@
 var h1 = document.querySelector('h1');
-var input = document.querySelector('input[type=text]');
 
-console.log("attribute names", input.getAttributeNames());
+var label = document.querySelector('label[type=text]');
