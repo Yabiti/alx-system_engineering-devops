c48e84fce0488d05d2e586ef66f5fb6edaa23cfb
Yabiti <yabetseyohannes@gmail.com>
2024-01-30 10:31:57 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,11 +1,3 @@
 var checkStatus = document.getElementById('checkStatus');
 
-var checkbox = document.getElementById('checkbox');
-
-checkbox.onchange = function(){
-    if(this.checked){
-    checkStatus.innerHTML = "checked!";
-    } else {
-        checkStatus.innerHTML = "Not Checked!";
-    }
-}
+var radio = document.getElementById('radio');
