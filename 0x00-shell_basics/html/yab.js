3ed3f395d8dc6836e73b5a40c2aaeaa7e5e5457e
Yabiti <yabetseyohannes@gmail.com>
2024-01-30 11:36:36 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -3,3 +3,7 @@ h1.style.color = "white";
 h1.style.border = "3px dashed blue";
 h1.style.textAlign = "center";
 h1.style.width = "50%";
+
+h1.onclick = function(){
+    this.classList.add("highlight");
+}
