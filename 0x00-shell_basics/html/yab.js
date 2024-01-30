694d6750d5f071ef4809f140c64e7efd6e0ee9b4
Yabiti <yabetseyohannes@gmail.com>
2024-01-30 10:03:23 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,13 +1,1 @@
-var button = document.querySelector('button');
-
-button.onmouseenter = function(){
-    this.innerHTML = "mouseentered!";
-
-button.ondblclick = function(){
-    this.innerHTML = "doubleclicked!";
-
-button.onmouseleave = function(){
-    this.innerHTML = "mouse left!";
-}
-}
-}
+var textid = document.querySelector('textid');
