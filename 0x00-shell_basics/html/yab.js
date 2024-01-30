930397f19a8ac88a588bbf63fb3106e6ffbb06ad
Yabiti <yabetseyohannes@gmail.com>
2024-01-30 10:57:39 +0300

    .

--- 0x00-shell_basics/html/yab.js
+++ 0x00-shell_basics/html/yab.js
@@ -1,11 +1,15 @@
-var checkStatus = document.getElementById('checkStatus');
+// write your code here
+var todoList = document.getElementById('todoList');
+var additems = document.getElementById('addTodo');
+additems.onclick = function(){
+    var todo_obj = document.getElementById("todo").value;
+    
+    if (todo_obj !== ""){
 
-var radio = document.getElementById('radio');
+        var li = document.createElement('li');
+        li.innerHTML = todo_obj;
+        todoList.appendChild(li);
+        document.getElementById("todo").value = "";
 
-radio.onchange = function(){
-    if(this.checked){
-        checkStatus.innerHTML = "Checked!";
-    } else {
-        checkStatus.innerHTML = "Not Checked!";
     }
 }
